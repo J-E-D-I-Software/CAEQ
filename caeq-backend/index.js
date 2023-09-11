802a85f82de43001faa5f44320cee6e779f806b4
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-09-11 13:52:48 -0600

    Folder structure and initial commit for backend and frontend

---
+++ caeq-backend/index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+
+const app = require(`${__dirname}/app.js`);
+
+const port = 3000;
+
+// app.listen nos regresa un objeto de
+const server = app.listen(port, () => {
+    console.log(`Server running on ${port}...`);
+});
+
+// UNHANDLED REJECTION
+/* Catching unhandled rejections. */
+process.on('unhandledRejection', (err) => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION!: SHUTTING DOWN');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+// SERVER SHUTDOWN
+/* A signal that is sent to the process to tell it to terminate. */
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received. Shutting down.');
+    server.close(() => {
+        console.log('Process terminated.');
+    });
+});

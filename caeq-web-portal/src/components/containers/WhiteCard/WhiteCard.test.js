d05307e152d32a4a069e3d3eabb5ac15b834a2ce
Edgar Ramirez <leramirezp@hotmail.com>
2023-09-13 17:17:09 -0600

    user/edgar/config-change-front setup for testing in react frontend

--- caeq-web-portal/src/components/containers/WhiteCard/WhiteCard.test.js
+++ caeq-web-portal/src/components/containers/WhiteCard/WhiteCard.test.js
@@ -1,15 +1,11 @@
 import { render, screen, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import WhiteCard from './WhiteCard';
 
 afterEach(() => {
     cleanup();
 });
 
-/* This is a test block, and it takes two params
- * @name: name of the test as a string
- * @assertions: function that will contain the assertions 
- */
 test('renders the WhiteCard component', () => {
     // We first need to load the component
     render(<WhiteCard />);

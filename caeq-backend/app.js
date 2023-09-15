0ec727ec97e9dfb9b7df1755a83b0a08a5142f7e
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-09-14 19:24:42 -0600

    Changed route name to caequser

--- caeq-backend/app.js
+++ caeq-backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/users', userRouter);
+app.use('/caequsers', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

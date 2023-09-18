529d23f3a9207b105ab27818016d4cf80c9765f2
jorgeCastro-CVJJ <cvjj@LAPTOP-I9T5HQLB>
2023-09-18 17:17:19 -0500

    Created Route.js

--- caeq-web-portal/src/App.js
+++ caeq-web-portal/src/App.js
@@ -1,67 +1,48 @@
 import "./styles/styles.scss";
-import WhiteContainer from "./components/containers/WhiteCard/WhiteCard";
-import TextInput from "./components/inputs/TextInput/TextInput";
-import LargeTextInput from "./components/inputs/TextInput/LargeTextInput";
-import DateInput from "./components/inputs/DateInput/DateInput";
-import HiddenTextInput from "./components/inputs/TextInput/HiddenTextInput";
-import InfoBox from "./components/cards/InfoBox";
-import Table from "./components/table/Table";
-import InteractiveTable from "./components/table/InteractiveTable";
-
-import DropdownInput from "./components/inputs/DropdownInput/DropdownInput";
-import FileInput from "./components/inputs/FileInput/FileInput";
-import { useState } from "react";
-import BaseButton from "./components/buttons/BaseButton";
-import NavbarButton from "./components/buttons/NavbarButton";
+import { Routes, Route } from 'react-router-dom';
+import routes from './routes';
 import Navbar from "./components/navbar/Navbar";
 
+import { useState } from "react";
+
 function App() {
-  const [inputVal, setVal] = useState("");
-  const [inputVal1, setVal2] = useState("");
-  const [inputVal2, setVal3] = useState("");
-  const options = ["Hombre", "Mujer", "Prefiero no decirlo"];
+
 
   return (
-    <div className="App">
-      <p>Bienvenido al CAEQ:</p>
-      <WhiteContainer>
-        <h1>Componentes</h1>
-        <p>{inputVal}</p>
-        <p>{inputVal1}</p>
-        <p>{inputVal2}</p>
-      </WhiteContainer>
-      <TextInput
-        label="Text"
-        getVal={inputVal}
-        setVal={setVal}
-        placeholder="Escibe aqui tu nombre"
-      />
-      <LargeTextInput
-        label="Large Text"
-        getVal={inputVal}
-        setVal={setVal}
-        placeholder="Escibe aqui tu nombre"
-      />
-      <DateInput label="Date" getVal={inputVal1} setVal={setVal2} />
-      <HiddenTextInput
-        label="Password"
-        placeholder="Contraseña de 8 a 16 caracteres"
-      />
-      <DropdownInput label="Select an option:" options={options} />
-      <FileInput label="File" getVal={inputVal2} setVal={setVal3} />
-      <InfoBox />
-      <Table />
-      <InteractiveTable />
-      <BaseButton label="Botón" />
-      <BaseButton label="Botón" type="primary" />
-      <BaseButton label="Botón" type="success" />
-      <BaseButton label="Botón" type="fail" />
-      <BaseButton label="Botón" type="disabled" />
-      {/* <NavbarButton label="Botón" type="navbar" /> */}
-      <Navbar/>
-
+    <div id='app-container'>
+      <div id='app-base-layout'>
+        <Routes>
+          {routes.map((route) =>
+            route.isPrivate ? (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <div>
+                    <Navbar />
+                    <route.Component />
+                  </div>
+                }
+              />
+            ) : (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <div>
+                    <Navbar />
+                    <route.Component />
+                  </div>
+                }
+              />
+            )
+          )} 
+        </Routes>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ caeq-web-portal/src/routes.js
@@ -0,0 +1,23 @@
+import Dasbboard from './screens/Dasboard';
+import Test from './screens/Test';
+
+
+import AdminIcon from "../src/components/icons/AdminIcon.png";
+import CursosIcon from "../src/components/icons/CursosIcon.png";
+import DirectorioIcon from "../src/components/icons/DirectorioIcon.png";
+import PrincipalIcon from "../src/components/icons/PrincipalIcon.png";
+
+
+const routes = [
+  { path: '/', name: 'Principal', Component: Dasbboard, icon: PrincipalIcon, isPrivate: false, inNavbar: true },
+    {
+        path: '/Cursos',
+        name: 'Cursos',
+        icon: CursosIcon,
+        Component: Test,
+        isPrivate: true,
+        inNavbar: true
+    }
+];
+
+export default routes;

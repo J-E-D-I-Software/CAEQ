f24eed43f586ea7da8f2690f485cb44945cbdd60
Edgar Ramirez <leramirezp@hotmail.com>
2023-09-21 12:37:34 -0600

    added the jwt library

--- caeq-web-portal/src/components/privateRoute/PrivateRoute.test.js
+++ caeq-web-portal/src/components/privateRoute/PrivateRoute.test.js
@@ -9,20 +9,49 @@ afterEach(() => {
 /* Test user is authenticated and component is rendered
  */
 test('Authenticated render', () => {
-	render(<PrivateRoute 
-        component={<h1 data-testid="authenticated"></h1>}
-    />);
+	render(<Routes>
+				<Route 
+					exact
+					path="/Login"
+					element={<h1 data-testid="login"></h1>}
+				/>
+				<Route 
+					exact
+					path="/prueba" 
+					element={
+						<PrivateRoute
+							component={<h1 data-testid="test-page"></h1>}
+						/>} 
+				/>
+			</Routes>
+	);
 
-	const element = screen.getByTestId('authenticated');
+	const element = screen.getByTestId('test');
 	expect(element).toBeInTheDocument();
 });
 
 /* Test user is not authenticated and component is not rendered
  */
 test('Authenticated render', () => {
-	render(<PrivateRoute 
-        component={<h1 data-testid="not-authenticated">Hello World</h1>}
-    />);
+	render(<Routes>
+				<Route 
+					exact
+					path="/Login"
+					element={<h1 data-testid="login"></h1>}
+				/>
+				<Route 
+					exact
+					path="/prueba" 
+					element={
+						isPrivate
+						?	<PrivateRoute
+								component={<h1 data-testid="test-page"></h1>}
+							/>
+						: 	<Component />
+					} 
+				/>
+			</Routes>
+	);	
 
 	const element = screen.getByTestId('not-authenticated');
 	expect(element).not.toBeInTheDocument();

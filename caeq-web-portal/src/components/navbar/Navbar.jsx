a439bd53eca48a6ef0947461f6fc4acd107f8c2f
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-09-27 20:06:57 -0600

    Documented components

--- caeq-web-portal/src/components/navbar/Navbar.jsx
+++ caeq-web-portal/src/components/navbar/Navbar.jsx
@@ -1,35 +1,43 @@
-import NavbarButton from "../buttons/NavbarButton";
-import BaseButton from "../buttons/BaseButton";
-import "./Navbar.scss";
-import routes from "../../routes";
-import { useNavigate } from "react-router-dom";
+import NavbarButton from '../buttons/NavbarButton';
+import BaseButton from '../buttons/BaseButton';
+import './Navbar.scss';
+import routes from '../../routes';
+import { useNavigate } from 'react-router-dom';
 
+/**
+ * Navbar component for navigation and user actions.
+ * @component
+ *
+ * @returns {JSX.Element} JSX element representing the Navbar.
+ *
+ * @example
+ * // Example usage of Navbar:
+ * <Navbar />
+ */
 const Navbar = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-
-    <div className="navbar">
-      <div className="navbar-button">
-        <BaseButton label="Cerrar sesión" type="fail" />
-      </div>
-      <div className="navbar-center">
-        {routes
-          .filter((route) => route.inNavbar)
-          .map((route) => (
-            <NavbarButton
-              label={route.name} 
-              key={route.path}
-              className='button-navbar'
-              id='inicio_btn'
-              icon={route.icon}
-              action={() => navigate(route.path)}/>
-              
-        ))}
-      </div>
-    </div>
-
-  );
+    return (
+        <div className='navbar'>
+            <div className='navbar-button'>
+                <BaseButton label='Cerrar sesión' type='fail' />
+            </div>
+            <div className='navbar-center'>
+                {routes
+                    .filter((route) => route.inNavbar)
+                    .map((route) => (
+                        <NavbarButton
+                            label={route.name}
+                            key={route.path}
+                            className='button-navbar'
+                            id='inicio_btn'
+                            icon={route.icon}
+                            action={() => navigate(route.path)}
+                        />
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;

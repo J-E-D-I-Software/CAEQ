1728f25acad569f79c5d83b48648e8cec7bc7bf1
Sarah Navarro <sarah@MacBook-Air-de-Sarah.local>
2023-10-02 00:12:00 -0600

    Created MiPerfil Screen and added route to routes.js

---
+++ caeq-web-portal/src/screens/Perfil/Perfil.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Perfil() {
+    return (
+        <div>
+            <h1>Mi Perfil</h1>
+        </div>
+    );
+}
+
+export default Perfil;

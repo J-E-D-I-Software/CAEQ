c276549a08962bf6f2a8d3b3d1d0ad851a24419c
jorgeCastro-CVJJ <a01707735@itesm.mx>
2023-10-03 02:02:49 -0500

    Added Prettier Format

--- caeq-web-portal/src/screens/DirectoryArchitectDetail/DirectoryArchitectDetails.js
+++ caeq-web-portal/src/screens/DirectoryArchitectDetail/DirectoryArchitectDetails.js
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { getArchitectUserById } from "../../client/ArchitectUser/ArchitectUser.GET";
 
 import TextInput from "../../components/inputs/TextInput/TextInput";
-import './DirectoryArchitectDetail.scss';
+import "./DirectoryArchitectDetail.scss";
 import FileInput from "../../components/inputs/FileInput/FileInput";
 import BaseButton from "../../components/buttons/BaseButton";
 
@@ -44,16 +44,16 @@ import BaseButton from "../../components/buttons/BaseButton";
 const ArchitectDetail = (props) => {
     const searchParams = useParams();
     const navigate = useNavigate();
-    const [data, setData] = useState({}); 
+    const [data, setData] = useState({});
 
     const dateOfAdmission = new Date(data.dateOfAdmission);
 
     useEffect(() => {
         if (searchParams.id)
             getArchitectUserById(searchParams.id)
-            .then(response => setData(response))
-            .catch(error => navigate('/404'));
-            console.log(data);
+                .then((response) => setData(response))
+                .catch((error) => navigate("/404"));
+        console.log(data);
     }, []);
 
     const today = new Date();
@@ -64,26 +64,65 @@ const ArchitectDetail = (props) => {
                 <h1>{data.fullName}</h1>
             </div>
 
-            
             <div className="architect-row">
-
                 <div className="architect-col">
-                    <TextInput label="Fecha de Ingreso" placeholder="FechaDeIngreso" getVal={dateOfAdmission.toLocaleDateString()} setVal={dateOfAdmission.toLocaleDateString()} />
-                    <TextInput label="Número de Colegiado" placeholder="Número de Colegiado" getVal={data.collegiateNumber} setVal={data.collegiateNumber} />
-                    <TextInput label="Número de DRO" placeholder="Número de DRO" getVal={data.DRONumber} setVal={data.DRONumber} />
-                    <TextInput label="Tipo de Miembro" placeholder="Tipo de Miembro" getVal={data.memberType} setVal={data.memberType} />
-                    <TextInput label="Especialidad" placeholder="Especialidad" getVal={data.specialty} setVal={data.specialty} />
+                    <TextInput
+                        label="Fecha de Ingreso"
+                        placeholder="FechaDeIngreso"
+                        getVal={dateOfAdmission.toLocaleDateString()}
+                        setVal={dateOfAdmission.toLocaleDateString()}
+                    />
+                    <TextInput
+                        label="Número de Colegiado"
+                        placeholder="Número de Colegiado"
+                        getVal={data.collegiateNumber}
+                        setVal={data.collegiateNumber}
+                    />
+                    <TextInput
+                        label="Número de DRO"
+                        placeholder="Número de DRO"
+                        getVal={data.DRONumber}
+                        setVal={data.DRONumber}
+                    />
+                    <TextInput
+                        label="Tipo de Miembro"
+                        placeholder="Tipo de Miembro"
+                        getVal={data.memberType}
+                        setVal={data.memberType}
+                    />
+                    <TextInput
+                        label="Especialidad"
+                        placeholder="Especialidad"
+                        getVal={data.specialty}
+                        setVal={data.specialty}
+                    />
                 </div>
-                
-                <div className="architect-col">
-                <TextInput label="Horas Acreditadas" placeholder="Horas Acreditadas" getVal={data.capacitationHours} setVal={data.capacitationHours} />
-                <TextInput label="Número de Asistencias a Asambleas" placeholder="Número de Asistencias a Asambleas" getVal={data.hoursAttended} setVal={data.hoursAttended} />
-                    
-                
-                <TextInput label="Pago de Anualidad" placeholder="Pago de Anualidad" getVal={data.uthorizationToShareInfo ? "Si": "No"} setVal={data.authorizationToShareInfo} />
 
-                <FileInput label="CV" placeholder="CV" getVal={""} setVal={""} />
-                <p>Archivo Actual: <a href="{data.linkCV}">{data.linkCV}</a></p>
+                <div className="architect-col">
+                    <TextInput
+                        label="Horas Acreditadas"
+                        placeholder="Horas Acreditadas"
+                        getVal={data.capacitationHours}
+                        setVal={data.capacitationHours}
+                    />
+                    <TextInput
+                        label="Número de Asistencias a Asambleas"
+                        placeholder="Número de Asistencias a Asambleas"
+                        getVal={data.hoursAttended}
+                        setVal={data.hoursAttended}
+                    />
+
+                    <TextInput
+                        label="Pago de Anualidad"
+                        placeholder="Pago de Anualidad"
+                        getVal={data.uthorizationToShareInfo ? "Si" : "No"}
+                        setVal={data.authorizationToShareInfo}
+                    />
+
+                    <FileInput label="CV" placeholder="CV" getVal={""} setVal={""} />
+                    <p>
+                        Archivo Actual: <a href="{data.linkCV}">{data.linkCV}</a>
+                    </p>
                 </div>
             </div>
 
@@ -91,9 +130,7 @@ const ArchitectDetail = (props) => {
                 <BaseButton type="primary">Guardar Cambios</BaseButton>
             </div>
         </div>
-       
-
     );
-}
+};
 
 export default ArchitectDetail;

4f389bac0afb6363f5b311e6efc5090aec6fc93b
Leonardo812 <a01707812@tec.mx>
2023-10-05 16:56:32 -0600

    Modified name of Password and Reset functions for Caeq and Architect Users

---
+++ caeq-web-portal/src/screens/ResetPasswordAdmin/ResetPasswordAdmin.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./ResetPasswordAdmin.scss";
+import HiddenTextInput from "../../components/inputs/TextInput/HiddenTextInput";
+import BaseButton from "../../components/buttons/BaseButton";
+import { FireError, FireSucess } from "../../utils/alertHandler";
+import { patchResetPasswordAdmin } from "../../client/CaeqUser/CaeqUser.PATCH";
+import { useParams, useNavigate } from "react-router-dom";
+
+const ResetPasswordAdmin = () => {
+    const { token } = useParams();
+    const [newpassword, setPassword] = useState("");
+    const [newpasswordConfirm, setConfirmPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleResetPasswordAdmin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await patchResetPasswordAdmin(
+                token,
+                newpassword,
+                newpasswordConfirm
+            );
+            if (response.status === "success") {
+                FireSucess(
+                    "Contraseña Restablecida con éxito, ya puede iniciar sesión"
+                );
+                navigate("/LoginAdmin");
+            }
+        } catch (error) {
+            FireError(error.response.data.message);
+        }
+    };
+
+    return (
+        <div className="reset-container">
+            <h2>Restablecer Contraseña</h2>
+            <div className="forgot-description">
+                <p2>
+                    Escribe tu nueva contraseña y confírmala. La contraseña debe tener al
+                    menos 8 caracteres.
+                </p2>
+            </div>
+
+            <form onSubmit={handleResetPasswordAdmin}>
+                <HiddenTextInput
+                    placeholder="Nueva Contraseña"
+                    getVal={newpassword}
+                    setVal={setPassword}
+                />
+                <HiddenTextInput
+                    placeholder="Confirmar Contraseña"
+                    getVal={newpasswordConfirm}
+                    setVal={setConfirmPassword}
+                />
+                <br />
+                <BaseButton type="primary" onClick={handleResetPasswordAdmin}>
+                    Restablecer Contraseña
+                </BaseButton>
+            </form>
+            <br />
+        </div>
+    );
+};
+
+export default ResetPasswordAdmin;
---
+++ caeq-web-portal/src/screens/ResetPasswordArchitect/ResetPasswordArchitect.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./ResetPasswordArchitect.scss";
+import HiddenTextInput from "../../components/inputs/TextInput/HiddenTextInput";
+import BaseButton from "../../components/buttons/BaseButton";
+import { FireError, FireSucess } from "../../utils/alertHandler";
+import { patchResetPasswordArchitec } from "../../client/ArchitectUser/ArchitecUser.PATCH";
+import { useParams, useNavigate } from "react-router-dom";
+
+const ResetPasswordArchitec = () => {
+    const { token } = useParams(); 
+    const [newpassword, setPassword] = useState("");
+    const [newpasswordConfirm, setConfirmPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleResetPasswordArchitec = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await patchResetPasswordArchitec(
+                token,
+                newpassword,
+                newpasswordConfirm
+            );
+            if (response.status === "success") {
+                FireSucess(
+                    "Contraseña Restablecida con éxito, ya puede iniciar sesión"
+                );
+                navigate("/LoginUser");
+            }
+        } catch (error) {
+            FireError(error.response.data.message);
+        }
+    };
+
+    return (
+        <div className="reset-container">
+            <h2>Restablecer Contraseña</h2>
+            <div className="forgot-description">
+                <p2>
+                    Escribe tu nueva contraseña y confírmala. La contraseña debe tener al
+                    menos 8 caracteres.
+                </p2>
+            </div>
+
+            <form onSubmit={handleResetPasswordArchitec}>
+                <HiddenTextInput
+                    placeholder="Nueva Contraseña"
+                    getVal={newpassword}
+                    setVal={setPassword}
+                />
+                <HiddenTextInput
+                    placeholder="Confirmar Contraseña"
+                    getVal={newpasswordConfirm}
+                    setVal={setConfirmPassword}
+                />
+                <br />
+                <BaseButton type="primary" onClick={handleResetPasswordArchitec}>
+                    Restablecer Contraseña
+                </BaseButton>
+            </form>
+            <br />
+        </div>
+    );
+};
+
+export default ResetPasswordArchitec;

11dc80935f40a6117f8d6752f5b457d6e8796070
Alan Razo <a01703350@itesm.mx>
2023-10-10 12:06:37 -0600

    Screens created

---
+++ caeq-web-portal/src/screens/Landing/LandingArchitect.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingArchitect = () => {
+    return <div>LandingArchitect</div>;
+};
+
+export default LandingArchitect;
---
+++ caeq-web-portal/src/screens/Landing/LandingCAEQ.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingCAEQ = () => {
+    return <div>LandingCAEQ</div>;
+};
+
+export default LandingCAEQ;

750d92034ef4c24bcc9dab96d7c50517cc2b797d
jorgeCastro-CVJJ <a01707735@itesm.mx>
2023-10-13 10:13:35 -0500

    Changed TextInputs into Dropdowns

--- caeq-web-portal/src/screens/DirectoryArchitectDetail/DirectoryArchitectDetails.js
+++ caeq-web-portal/src/screens/DirectoryArchitectDetail/DirectoryArchitectDetails.js
@@ -16,21 +16,19 @@ const ArchitectDetail = (props) => {
     const [data, setData] = useState({});
     const [editedData, setEditedData] = useState({});
 
-    
     useEffect(() => {
         if (searchParams.id)
-        getArchitectUserById(searchParams.id)
-    .then((response) => {
-        setData(response)
-        setEditedData(response)
-    })
-    .catch((error) => navigate("/404"));
-    console.log(data);
-}, []);
+            getArchitectUserById(searchParams.id)
+                .then((response) => {
+                    setData(response);
+                    setEditedData(response);
+                })
+                .catch((error) => navigate("/404"));
+        console.log(data);
+    }, []);
 
     const handleSaveChanges = () => {
-
-        try{
+        try {
             updateArchitectUserByID(searchParams.id, editedData)
                 .then((response) => {
                     console.log(response);
@@ -39,11 +37,11 @@ const ArchitectDetail = (props) => {
                 .catch((error) => {
                     console.log("Failed to update user:", error);
                 });
-            FireSucess('Los Cambios se han guardado correctamente')
+            FireSucess("Los Cambios se han guardado correctamente");
             navigate("/Directorio");
         } catch (error) {
             console.log(error);
-        };
+        }
     };
 
     return (
@@ -58,32 +56,47 @@ const ArchitectDetail = (props) => {
                         label="Fecha de Ingreso"
                         placeholder="FechaDeIngreso"
                         getVal={editedData.dateOfAdmission}
-                        setVal={(value) => setEditedData({ ...editedData, dateOfAdmission: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, dateOfAdmission: value })
+                        }
                     />
                     <TextInput
                         label="Número de Colegiado"
                         placeholder="Número de Colegiado"
                         getVal={editedData.collegiateNumber}
-                        setVal={(value) => setEditedData({ ...editedData, collegiateNumber: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, collegiateNumber: value })
+                        }
                     />
                     <TextInput
                         label="Número de DRO"
                         placeholder="Número de DRO"
                         getVal={editedData.DRONumber}
-                        setVal={(value) => setEditedData({ ...editedData, DRONumber: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, DRONumber: value })
+                        }
                     />
-                    <TextInput
+                    <DropdownInput
                         label="Tipo de Miembro"
-                        placeholder="Tipo de Miembro"
+                        options={[
+                            "Miembro de número",
+                            "Miembro Adherente",
+                            "Miembro Pasante",
+                            "Miembro Honorario",
+                            "Miembro Vitalicio",
+                        ]}
                         getVal={editedData.memberType}
-                        setVal={(value) => setEditedData({ ...editedData, memberType:value})}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, memberType: value })
+                        }
                     />
-
                     <TextInput
                         label="Especialidad"
                         placeholder="Especialidad"
                         getVal={editedData.specialty}
-                        setVal={(value) => setEditedData({ ...editedData, specialty: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, specialty: value })
+                        }
                     />
                 </div>
 
@@ -92,25 +105,39 @@ const ArchitectDetail = (props) => {
                         label="Horas Acreditadas"
                         placeholder="Horas Acreditadas"
                         getVal={editedData.capacitationHours}
-                        setVal={(value) => setEditedData({ ...editedData, capacitationHours: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, capacitationHours: value })
+                        }
                     />
                     <TextInput
                         label="Número de Asistencias a Asambleas"
                         placeholder="Número de Asistencias a Asambleas"
                         getVal={editedData.hoursAttended}
-                        setVal={(value) => setEditedData({ ...editedData, hoursAttended: value })}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, hoursAttended: value })
+                        }
                     />
 
                     <DropdownInput
                         label="Pago de Anualidad"
                         getVal={editedData.authorizationToShareInfo}
-                        setVal={(value) => setEditedData({...editedData, authorizationToShareInfo: value === "Si" ? true : false})}
+                        setVal={(value) =>
+                            setEditedData({
+                                ...editedData,
+                                authorizationToShareInfo: value === "Si" ? true : false,
+                            })
+                        }
                         options={["Si", "No"]}
-                        placeholder="Pago de Anualidad"
                     />
-                    
 
-                    <FileInput label="CV" placeholder="CV" getVal={data.linkCV} setVal={(value) => setEditedData({ ...editedData, linkCV: value})} />
+                    <FileInput
+                        label="CV"
+                        placeholder="CV"
+                        getVal={data.linkCV}
+                        setVal={(value) =>
+                            setEditedData({ ...editedData, linkCV: value })
+                        }
+                    />
                     <p>
                         Archivo Actual: <a href="{data.linkCV}">{data.linkCV}</a>
                     </p>

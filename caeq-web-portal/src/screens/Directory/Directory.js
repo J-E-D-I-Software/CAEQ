77feb417b117028ba5581de73b0f379111ca8389
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-10-17 01:57:15 -0600

    Added header mappings and download button

--- caeq-web-portal/src/screens/Directory/Directory.js
+++ caeq-web-portal/src/screens/Directory/Directory.js
@@ -1,127 +1,165 @@
-import React, { useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
-import InteractiveTable from "../../components/table/InteractiveTable";
-import InputText from "../../components/inputs/TextInput/TextInput";
-import { getAllArchitectUsers } from "../../client/ArchitectUser/ArchitectUser.GET";
-import PaginationNav from "../../components/pagination/PaginationNav";
-import "./directory.scss";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import InteractiveTable from '../../components/table/InteractiveTable';
+import InputText from '../../components/inputs/TextInput/TextInput';
+import { getAllArchitectUsers } from '../../client/ArchitectUser/ArchitectUser.GET';
+import PaginationNav from '../../components/pagination/PaginationNav';
+import { exportToExcel } from 'react-json-to-excel';
+import headerMappings from '../../components/table/HeaderMappings';
+import './directory.scss';
 /**
  * Componente que muestra una lista de arquitectos con función de búsqueda.
  * @component
  */
 const Directory = () => {
-  const [architectUsers, setArchitectUsers] = useState([]);
-  const [getArchitect, setArchitect] = useState("");
-  const [paginationPage, setPaginationPage] = useState(1);
-  const navigate = useNavigate();
+    const [architectUsers, setArchitectUsers] = useState([]);
+    const [getArchitect, setArchitect] = useState('');
+    const [paginationPage, setPaginationPage] = useState(1);
+    const navigate = useNavigate();
 
+    const handleRowClick = (id) => {
+        // Utiliza navigate para redirigir a la página de detalles del directorio
+        navigate(`/Directorio/${id}`);
+    };
 
-  const handleRowClick = (id) => {
-    // Utiliza navigate para redirigir a la página de detalles del directorio
-    navigate(`/Directorio/${id}`);
-  };
+    /**
+     * Efecto que se ejecuta al cargar el componente para obtener la lista completa de arquitectos.
+     */
 
-  /**
-   * Efecto que se ejecuta al cargar el componente para obtener la lista completa de arquitectos.
-   */
+    useEffect(() => {
+        (async () => {
+            try {
+                let filters = '';
+                const architects = await getAllArchitectUsers(paginationPage, filters);
+                setArchitectUsers(architects);
+            } catch (error) {}
+        })();
+    }, [paginationPage]);
+    /**
+     * Función que filtra los arquitectos en función del texto de búsqueda.
+     * @param {Object[]} data - La lista de arquitectos completa.
+     * @param {string} searchText - El texto de búsqueda.
+     * @returns {Object[]} - La lista de arquitectos filtrada.
+     */
+    const filterArchitects = (data, searchText) => {
+        // Filtrar los arquitectos en función del texto de búsqueda
+        return data.filter((architect) => {
+            // Convertir todos los valores de los arquitectos en cadenas de texto
+            const architectValues = Object.values(architect).map((value) =>
+                String(value).toLowerCase()
+            );
+            // Verificar si algún valor contiene el texto de búsqueda
+            return architectValues.some((value) =>
+                value.includes(searchText.toLowerCase())
+            );
+        });
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        let filters = "";
-        const architects = await getAllArchitectUsers(paginationPage, filters);
-        setArchitectUsers(architects);
-      } catch (error) {}
-    })();
-  }, [paginationPage]);
-  /**
-   * Función que filtra los arquitectos en función del texto de búsqueda.
-   * @param {Object[]} data - La lista de arquitectos completa.
-   * @param {string} searchText - El texto de búsqueda.
-   * @returns {Object[]} - La lista de arquitectos filtrada.
-   */
-  const filterArchitects = (data, searchText) => {
     // Filtrar los arquitectos en función del texto de búsqueda
-    return data.filter((architect) => {
-      // Convertir todos los valores de los arquitectos en cadenas de texto
-      const architectValues = Object.values(architect).map((value) =>
-        String(value).toLowerCase()
-      );
-      // Verificar si algún valor contiene el texto de búsqueda
-      return architectValues.some((value) =>
-        value.includes(searchText.toLowerCase())
-      );
-    });
-  };
+    const filteredArchitects = filterArchitects(architectUsers, getArchitect);
 
-  // Filtrar los arquitectos en función del texto de búsqueda
-  const filteredArchitects = filterArchitects(architectUsers, getArchitect);
-
-  // Filtrar y excluir la última columna antes de pasar los datos a InteractiveTable
-  const tablefilteredArchitects = filteredArchitects
-  /*.map(
+    // Filtrar y excluir la última columna antes de pasar los datos a InteractiveTable
+    const tablefilteredArchitects = filteredArchitects;
+    /*.map(
     ({ _id, ...rest }) => rest
   );*/
-  const columnsToShow =
-    filteredArchitects?.length > 0 ? Object.keys(filteredArchitects[0]) : [];
+    const columnsToShow =
+        filteredArchitects?.length > 0 ? Object.keys(filteredArchitects[0]) : [];
 
-  if (columnsToShow.length > 1) {
-    filteredArchitects.forEach((architect) => {
-      // Elimina la última propiedad de cada objeto arquitecto
-      delete architect[columnsToShow[columnsToShow.length - 1]];
-    });
-  }
-
-  /**
-   * Maneja la acción de retroceder a la página anterior en la paginación.
-   */
-
-  const handlePreviousPage = () => {
-    if (paginationPage > 1) {
-      setPaginationPage(paginationPage - 1);
+    if (columnsToShow.length > 1) {
+        filteredArchitects.forEach((architect) => {
+            // Elimina la última propiedad de cada objeto arquitecto
+            delete architect[columnsToShow[columnsToShow.length - 1]];
+        });
     }
-  };
 
-  /**
-   * Maneja la acción de avanzar a la siguiente página en la paginación.
-   */
-  const handleNextPage = () => {
-    setPaginationPage(paginationPage + 1);
-  };
+    /**
+     * Maneja la acción de retroceder a la página anterior en la paginación.
+     */
 
-  return (
-    console.log("this is",tablefilteredArchitects[0]),
-    <div className="directory">
-      <div className="directory-row directory-header">
-        <h1>Directorio de arquitectos</h1>
-      </div>
-      <label>
-        <InputText
-          getVal={getArchitect}
-          setVal={setArchitect}
-          placeholder="Buscar"
-        />
-      </label>
+    const handlePreviousPage = () => {
+        if (paginationPage > 1) {
+            setPaginationPage(paginationPage - 1);
+        }
+    };
 
-      <div className="directory-row">
-        {filteredArchitects.length > 0 ? (
-          <div className="box-container">
-            <InteractiveTable data={filteredArchitects} onRowClick={handleRowClick} />
-          </div>
-        ) : (
-          <p className="no-data-message">No hay colegiados disponibles</p>
-        )}
-      </div>
+    /**
+     * Maneja la acción de avanzar a la siguiente página en la paginación.
+     */
+    const handleNextPage = () => {
+        setPaginationPage(paginationPage + 1);
+    };
 
-      <div className="directory-row directory-pagination">
-        <PaginationNav
-          onClickBefore={handlePreviousPage}
-          onClickAfter={handleNextPage}
-          page={paginationPage}
-        />
-      </div>
-    </div>
-  );
+    const handleDownload = async () => {
+        const architects = await getAllArchitectUsers(paginationPage, {}, 10000);
+
+        const architectsDownload = architects.map((val) => {
+            delete val._id;
+
+            // Create a new object with mapped headers
+            const mappedObject = {};
+            for (const key in val) {
+                if (headerMappings[key]) {
+                    mappedObject[headerMappings[key]] = val[key];
+
+                    if (
+                        typeof mappedObject[headerMappings[key]] === 'boolean' &&
+                        mappedObject[headerMappings[key]] === true
+                    ) {
+                        mappedObject[headerMappings[key]] = 'Si';
+                    } else if (
+                        typeof mappedObject[headerMappings[key]] === 'boolean' &&
+                        mappedObject[headerMappings[key]] === false
+                    ) {
+                        mappedObject[headerMappings[key]] = 'No';
+                    }
+                }
+            }
+
+            return mappedObject;
+        });
+        exportToExcel(architectsDownload, 'seleccion-arquitectos', false);
+    };
+
+    return (
+        console.log('this is', tablefilteredArchitects[0]),
+        (
+            <div className='directory'>
+                <div className='directory-row directory-header'>
+                    <h1>Directorio de arquitectos</h1>
+                </div>
+                <button onClick={() => handleDownload()}>Descargar arquitectos</button>
+                <label>
+                    <InputText
+                        getVal={getArchitect}
+                        setVal={setArchitect}
+                        placeholder='Buscar'
+                    />
+                </label>
+
+                <div className='directory-row'>
+                    {filteredArchitects.length > 0 ? (
+                        <div className='box-container'>
+                            <InteractiveTable
+                                data={filteredArchitects}
+                                onRowClick={handleRowClick}
+                            />
+                        </div>
+                    ) : (
+                        <p className='no-data-message'>No hay colegiados disponibles</p>
+                    )}
+                </div>
+
+                <div className='directory-row directory-pagination'>
+                    <PaginationNav
+                        onClickBefore={handlePreviousPage}
+                        onClickAfter={handleNextPage}
+                        page={paginationPage}
+                    />
+                </div>
+            </div>
+        )
+    );
 };
 
 export default Directory;

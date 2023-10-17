2a25269abf2574f54bf0409b1f4143ddc8531e5b
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-10-17 12:15:25 -0600

    Integrated new filters

--- caeq-web-portal/src/screens/Directory/Directory.js
+++ caeq-web-portal/src/screens/Directory/Directory.js
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import DropdownInput from '../../components/inputs/DropdownInput/DropdownInput';
-import InteractiveTable from "../../components/table/InteractiveTable";
-import InputText from "../../components/inputs/TextInput/TextInput";
-import { getAllArchitectUsers } from "../../client/ArchitectUser/ArchitectUser.GET";
-import PaginationNav from "../../components/pagination/PaginationNav";
-import "./directory.scss";
+import InteractiveTable from '../../components/table/InteractiveTable';
+import InputText from '../../components/inputs/TextInput/TextInput';
+import { getAllArchitectUsers } from '../../client/ArchitectUser/ArchitectUser.GET';
+import PaginationNav from '../../components/pagination/PaginationNav';
+import headerMappings from '../../components/table/HeaderMappings';
+import { exportToExcel } from 'react-json-to-excel';
+import './directory.scss';
 
 const Directory = () => {
-  const [architectUsers, setArchitectUsers] = useState([]);
-  const [filterSearchByName, setFilterSearchByName] = useState('');
-  const [filterSearchBymunicipalityOfLabor, setFilterSearchBymunicipalityOfLabor] = useState('');
-  const [filterSearchByDRONumber, setFilterSearchByDRONumber] = useState('');
-  const [filtergender, setFiltergender] = useState('');
-  const [filterclassification, setFilterclassification] = useState('');
-  const [filtermemberType, setFiltermemberType] = useState('');
-  const [paginationPage, setPaginationPage] = useState(1);
-  const navigate = useNavigate();
+    const [architectUsers, setArchitectUsers] = useState([]);
+    const [filterSearchByName, setFilterSearchByName] = useState('');
+    const [filterSearchBymunicipalityOfLabor, setFilterSearchBymunicipalityOfLabor] =
+        useState('');
+    const [filterSearchByDRONumber, setFilterSearchByDRONumber] = useState('');
+    const [filtergender, setFiltergender] = useState('');
+    const [filterclassification, setFilterclassification] = useState('');
+    const [filtermemberType, setFiltermemberType] = useState('');
+    const [paginationPage, setPaginationPage] = useState(1);
+    const navigate = useNavigate();
 
-  const handleRowClick = (id) => {
-    navigate(`/Directorio/${id}`);
-  };
+    const handleRowClick = (id) => {
+        navigate(`/Directorio/${id}`);
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        let filters = "";
+    const calculateFilters = () => {
+        let filters = '';
         if (filterSearchByName) filters = `fullName[regex]=${filterSearchByName}`;
-        if (filterSearchBymunicipalityOfLabor) filters += `&municipalityOfLabor[regex]=${filterSearchBymunicipalityOfLabor}`;
-        if (filterSearchByDRONumber) filters += `&DRONumber[regex]=${filterSearchByDRONumber}`;
+        if (filterSearchBymunicipalityOfLabor)
+            filters += `&municipalityOfLabor[regex]=${filterSearchBymunicipalityOfLabor}`;
+        if (filterSearchByDRONumber)
+            filters += `&DRONumber[regex]=${filterSearchByDRONumber}`;
         if (filtergender) filters += `&gender=${filtergender}`;
         if (filterclassification) filters += `&classification=${filterclassification}`;
         if (filtermemberType) filters += `&memberType=${filtermemberType}`;
+        return filters;
+    };
 
-        const architects = await getAllArchitectUsers(paginationPage, filters);
-        setArchitectUsers(architects);
-      } catch (error) {
-        // Handle error
-      }
-    })();
-  }, [paginationPage, filterSearchByName, filterSearchBymunicipalityOfLabor, filterSearchByDRONumber, filtergender, filterclassification, filtermemberType]);
-
-  const handlePreviousPage = () => {
-    if (paginationPage > 1) {
-      setPaginationPage(paginationPage - 1);
-    }
-
-    /**
-     * Maneja la acción de retroceder a la página anterior en la paginación.
-     */
+    useEffect(() => {
+        (async () => {
+            try {
+                const filters = calculateFilters();
+                const architects = await getAllArchitectUsers(paginationPage, filters);
+                setArchitectUsers(architects);
+            } catch (error) {
+                // Handle error
+            }
+        })();
+    }, [
+        paginationPage,
+        filterSearchByName,
+        filterSearchBymunicipalityOfLabor,
+        filterSearchByDRONumber,
+        filtergender,
+        filterclassification,
+        filtermemberType,
+    ]);
 
     const handlePreviousPage = () => {
         if (paginationPage > 1) {
@@ -56,15 +64,13 @@ const Directory = () => {
         }
     };
 
-    /**
-     * Maneja la acción de avanzar a la siguiente página en la paginación.
-     */
     const handleNextPage = () => {
         setPaginationPage(paginationPage + 1);
     };
 
     const handleDownload = async () => {
-        const architects = await getAllArchitectUsers(paginationPage, '', 10000);
+        const filters = calculateFilters();
+        const architects = await getAllArchitectUsers(paginationPage, filters, 10000);
 
         console.log(architects);
 
@@ -98,49 +104,81 @@ const Directory = () => {
             return mappedObject;
         });
 
-        console.log(architectsDownload);
-
         exportToExcel(architectsDownload, 'seleccion-arquitectos', false);
     };
 
     return (
-        console.log('this is', tablefilteredArchitects[0]),
-        (
-            <div className='directory'>
-                <div className='directory-row directory-header'>
-                    <h1>Directorio de arquitectos</h1>
-                </div>
-                <button onClick={() => handleDownload()}>Descargar arquitectos</button>
-                <label>
-                    <InputText
-                        getVal={getArchitect}
-                        setVal={setArchitect}
-                        placeholder='Buscar'
-                    />
-                </label>
-
-                <div className='directory-row'>
-                    {filteredArchitects.length > 0 ? (
-                        <div className='box-container'>
-                            <InteractiveTable
-                                data={filteredArchitects}
-                                onRowClick={handleRowClick}
-                            />
-                        </div>
-                    ) : (
-                        <p className='no-data-message'>No hay colegiados disponibles</p>
-                    )}
-                </div>
-
-                <div className='directory-row directory-pagination'>
-                    <PaginationNav
-                        onClickBefore={handlePreviousPage}
-                        onClickAfter={handleNextPage}
-                        page={paginationPage}
-                    />
-                </div>
+        <div className='directory'>
+            <div className='directory-row directory-header'>
+                <h1>Directorio de arquitectos</h1>
             </div>
-        )
+            <button onClick={() => handleDownload()}>Descargar arquitectos</button>
+
+            <DropdownInput
+                getVal={filtergender}
+                setVal={setFiltergender}
+                options={['Hombre', 'Mujer', 'Prefiero no decirlo']}
+                placeholder='Filtrar género'
+            />
+
+            <DropdownInput
+                getVal={filterclassification}
+                setVal={setFilterclassification}
+                options={['Expresidente', 'Docente', 'Convenio']}
+                placeholder='Filtrar clasificación'
+            />
+
+            <DropdownInput
+                getVal={filtermemberType}
+                setVal={setFiltermemberType}
+                options={[
+                    'Miembro de número',
+                    'Miembro Adherente',
+                    'Miembro Pasante',
+                    'Miembro Vitalicio',
+                    'Miembro Honorario',
+                ]}
+                placeholder='Filtrar tipo de miembro'
+            />
+
+            <InputText
+                placeholder='Nombre del colegiado'
+                getVal={filterSearchByName}
+                setVal={setFilterSearchByName}
+            />
+
+            <InputText
+                placeholder='Municipio'
+                getVal={filterSearchBymunicipalityOfLabor}
+                setVal={setFilterSearchBymunicipalityOfLabor}
+            />
+            <InputText
+                placeholder='Número de DRO'
+                getVal={filterSearchByDRONumber}
+                setVal={setFilterSearchByDRONumber}
+            />
+
+            <div className='directory-row'>
+                {architectUsers.length > 0 ? (
+                    <div className='box-container'>
+                        <InteractiveTable
+                            data={architectUsers}
+                            onRowClick={handleRowClick}
+                        />
+                    </div>
+                ) : (
+                    <p className='no-data-message'>No hay colegiados disponibles</p>
+                )}
+            </div>
+
+            <div className='directory-row directory-pagination'>
+                <PaginationNav
+                    onClickBefore={handlePreviousPage}
+                    onClickAfter={handleNextPage}
+                    page={paginationPage}
+                />
+            </div>
+        </div>
     );
 };
 

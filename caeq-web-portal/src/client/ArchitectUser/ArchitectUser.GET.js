46185f2ea6a9196a82a6a5366585db48cd1ee3c3
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-10-28 02:25:55 -0600

    Fixed watson 51

--- caeq-web-portal/src/client/ArchitectUser/ArchitectUser.GET.js
+++ caeq-web-portal/src/client/ArchitectUser/ArchitectUser.GET.js
@@ -6,8 +6,12 @@ const paginationPageLimit = process.env.PAGINATION_PAGE_LIMIT || 100;
  * It makes a GET request to the endpoint `/architectusers` and returns the response data.
  * @returns An array of objects.
  */
-export async function getAllArchitectUsers(page = 1, filtersParams = '') {
-    let endpoint = `${baseApiEndpoint}/architectusers?page=${page}&limit=${paginationPageLimit}&${filtersParams}`;
+export async function getAllArchitectUsers(
+    page = 1,
+    filtersParams = '',
+    pageLimit = 100
+) {
+    let endpoint = `${baseApiEndpoint}/architectusers?page=${page}&limit=${pageLimit}&${filtersParams}`;
 
     const response = await axios.get(endpoint);
     return response.data.data.documents;

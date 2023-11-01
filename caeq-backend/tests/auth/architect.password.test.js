82bbd73d3ea86e359f4852d44ae8ebabbda3fa1d
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-10-31 19:55:47 -0600

    Removed password Unit Tests

--- caeq-backend/tests/auth/architect.password.test.js
+++ caeq-backend/tests/auth/architect.password.test.js
@@ -1,42 +1,42 @@
-const request = require('supertest');
-const app = require('../../app');
-const { connectDB } = require('../config/databaseTest');
-const { setUpDbWithMuckData } = require('../../models/testdata.setup');
-const agent = request.agent(app);
+// const request = require('supertest');
+// const app = require('../../app');
+// const { connectDB } = require('../config/databaseTest');
+// const { setUpDbWithMuckData } = require('../../models/testdata.setup');
+// const agent = request.agent(app);
 
-beforeAll(async () => {
-    await connectDB();
-    await setUpDbWithMuckData();
-});
+// beforeAll(async () => {
+//     await connectDB();
+//     await setUpDbWithMuckData();
+// });
 
-const ForgotPasswordArchitect = async () => {
-    // Request to endpoint
-    const resTest1 = await agent.post('/architectusers/forgot-password').send({
-        email: 'relisib653@mugadget.com',
-    });
+// const ForgotPasswordArchitect = async () => {
+//     // Request to endpoint
+//     const resTest1 = await agent.post('/architectusers/forgot-password').send({
+//         email: 'relisib653@mugadget.com',
+//     });
 
-    const resTest2 = await agent.post('/architectusers/forgot-password').send({
-        email: 'leo9ramosp@hotmail.com',
-    });
+//     const resTest2 = await agent.post('/architectusers/forgot-password').send({
+//         email: 'leo9ramosp@hotmail.com',
+//     });
 
-    // Assertions
-    expect(resTest1.statusCode).toEqual(200);
-    expect(resTest2.statusCode).toEqual(404);
-};
+//     // Assertions
+//     expect(resTest1.statusCode).toEqual(200);
+//     expect(resTest2.statusCode).toEqual(404);
+// };
 
-const ResetPasswordArchitect = async () => {
-    let response = await agent.post('/architectusers/forgot-password').send({
-        email: 'relisib653@mugadget.com',
-    });
-    const token = response.body.data.resetToken;
+// const ResetPasswordArchitect = async () => {
+//     let response = await agent.post('/architectusers/forgot-password').send({
+//         email: 'relisib653@mugadget.com',
+//     });
+//     const token = response.body.data.resetToken;
 
-    response = await agent.patch(`/architectusers/reset-password/${token}`).send({
-        [token]: token,
-        password: '123456789',
-        passwordConfirm: '123456789',
-    });
-    expect(response.statusCode).toEqual(200);
-};
+//     response = await agent.patch(`/architectusers/reset-password/${token}`).send({
+//         [token]: token,
+//         password: '123456789',
+//         passwordConfirm: '123456789',
+//     });
+//     expect(response.statusCode).toEqual(200);
+// };
 
 // describe('Architect forgot Password succesful', () => {
 //     test('successful', () => ForgotPasswordArchitect());

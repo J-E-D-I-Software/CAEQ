106e7d7cca3810fb5679fe995bb3578b6afeb314
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-11-01 19:10:09 -0600

    Fixed title in landing

--- caeq-web-portal/src/screens/Landing/LandingArchitect.js
+++ caeq-web-portal/src/screens/Landing/LandingArchitect.js
@@ -17,9 +17,7 @@ const LandingArchitect = () => {
             <div className='welcome'>
                 <div className='column left-column'>
                     <img src={Logo} alt='Logo' className='logo' />
-                    <div className='titlecaeq'>
-                        Bienvenidos al portal de colegiados
-                    </div>
+                    <div className='titlecaeq'>Bienvenidos al portal de CAEQ</div>
                     <p className='subtitlecaeq'>
                         Un nuevo espacio de consulta informativa.
                     </p>
@@ -36,9 +34,7 @@ const LandingArchitect = () => {
                 <div className='column right-column'>
                     <div className='adminbutton'>
                         <Link to='/welcomeAdmin'>
-                            <BaseButton type='disabled'>
-                                Familia CAEQ
-                            </BaseButton>
+                            <BaseButton type='disabled'>Familia CAEQ</BaseButton>
                         </Link>
                     </div>
                     <img src={Image1} alt='Image1' className='image1' />
@@ -51,9 +47,9 @@ const LandingArchitect = () => {
                 <div className='column right-column'>
                     <div className='titleOne'>C A E Q</div>
                     <div className='descOne'>
-                        Espacio de expresión, opinión, colaboración y
-                        capacitación para promover e impulsar el mejoramiento
-                        arquitectónico y urbano de Querétaro.
+                        Espacio de expresión, opinión, colaboración y capacitación para
+                        promover e impulsar el mejoramiento arquitectónico y urbano de
+                        Querétaro.
                     </div>
                 </div>
             </div>
@@ -61,8 +57,8 @@ const LandingArchitect = () => {
                 <div className='column left-column'>
                     <div className='titleTwo'>Objetivos</div>
                     <div className='descTwo'>
-                        Integrar a los profesionistas de la arquitectura,
-                        promoviendo, la participación dentro del gremio.
+                        Integrar a los profesionistas de la arquitectura, promoviendo, la
+                        participación dentro del gremio.
                     </div>
                 </div>
                 <div className='column right-column'>

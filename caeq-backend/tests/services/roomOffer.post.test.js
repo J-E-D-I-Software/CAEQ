1cb7f60d7c443133fb6331c5669fa97e389c9120
Jose Eduardo Diaz Maldonado <A01735676@tec.mx>
2023-11-03 18:41:22 -0600

    test updated

--- caeq-backend/tests/services/roomOffer.post.test.js
+++ caeq-backend/tests/services/roomOffer.post.test.js
@@ -1,4 +1,3 @@
-/*
 const request = require('supertest');
 const app = require('../../app');
 const { connectDB } = require('../config/databaseTest');
@@ -11,8 +10,13 @@ const testPostRoomOffer = async () => {
     const endpoint = '/services';
 
     let res = await agent.post(endpoint).send({
+        name: 'Salón Epigmenio Martínez',
+        cost: 1200,
+        capacity: 20,
+        specifications: 'Aire acondicionado, ',
+        roomPhoto: null,
 
     })
 
 }
-*/
+

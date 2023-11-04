72413bd60c223b91a86bb18c6aa2112a09718221
Jose Eduardo Diaz Maldonado <A01735676@tec.mx>
2023-11-03 18:34:40 -0600

    Named tests for post of room offer

---
+++ caeq-backend/tests/services/roomOffer.post.test.js
@@ -0,0 +1,18 @@
+/*
+const request = require('supertest');
+const app = require('../../app');
+const { connectDB } = require('../config/databaseTest');
+const { loginAdmin } = require('../config/authSetUp');
+const { setUpDbWithMuckData } = require('../../models/testdata.setup');
+
+const agent = request.agent(app);
+
+const testPostRoomOffer = async () => {
+    const endpoint = '/services';
+
+    let res = await agent.post(endpoint).send({
+
+    })
+
+}
+*/

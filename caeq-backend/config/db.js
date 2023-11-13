a1f115064e0ded53558e27fcd17d598630b9e022
Edgar Ramirez <leramirezp@hotmail.com>
2023-11-13 17:09:19 -0600

    .

--- caeq-backend/config/db.js
+++ caeq-backend/config/db.js
@@ -7,6 +7,7 @@ const {
 // Creates and initializes a Firebase app instance. Pass options as param
 const admin = require('firebase-admin');
 const credentials = require(`../keys/${GOOGLE_CREDENTIALS_FILE}`);
+console.log(credentials);
 
 const db = admin.initializeApp({
     credential: admin.credential.cert(credentials),

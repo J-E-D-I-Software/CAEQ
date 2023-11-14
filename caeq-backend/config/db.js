f7e221d571125d47236fbe97f4a843fa55b2e0a7
Edgar Ramirez <leramirezp@hotmail.com>
2023-11-13 19:14:45 -0600

    .

--- caeq-backend/config/db.js
+++ caeq-backend/config/db.js
@@ -1,17 +1,28 @@
 // Importing our configuration to initialize our app
 const { 
     STORAGE_BUCKET,
-    GOOGLE_CREDENTIALS_FILE 
+    GOOGLE_CREDENTIALS_FILE,
+    NODE_ENV
 } = process.env;
 
 // Creates and initializes a Firebase app instance. Pass options as param
-const admin = require('firebase-admin');
-const credentials = require(`../keys/${GOOGLE_CREDENTIALS_FILE}`);
-console.log(credentials);
+let admin;
+let db;
 
-const db = admin.initializeApp({
-    credential: admin.credential.cert(credentials),
-    storageBucket: STORAGE_BUCKET,
-});
+if (NODE_ENV !== 'test') {
+    // Only initialize Firebase if not running tests
+    admin = require('firebase-admin');
+    const credentials = require(`../keys/${GOOGLE_CREDENTIALS_FILE}`);
+    
+    db = admin.initializeApp({
+        credential: admin.credential.cert(credentials),
+        storageBucket: STORAGE_BUCKET,
+    });
+} else {
+    // For tests, you might want to use a mock or a different configuration
+    // For example, you can use the Firebase Emulator Suite or a mock library
+    admin = require('firebase-admin/lib/mocks');
+    db = admin.initializeApp();
+}
 
 module.exports = db;

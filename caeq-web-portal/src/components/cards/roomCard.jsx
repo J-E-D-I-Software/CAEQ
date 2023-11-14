6385e7ba8155410722031996e06562eb71b4d673
Castro J. Jorge <A01707735@itesm.mx>
2023-11-14 08:47:09 -0600

    Added BurguerMenu when on phone view and CurrencyFormat added on courses and services

--- caeq-web-portal/src/components/cards/roomCard.jsx
+++ caeq-web-portal/src/components/cards/roomCard.jsx
@@ -3,6 +3,7 @@ import BaseButton from '../buttons/BaseButton';
 import RestrictByRole from '../restrictAccess/RestrictByRole';
 import PhotoTemplate from '../../components/images/salon_foto.jpg'
 import { FireError, FireLoading, FireSucess } from '../../utils/alertHandler';
+import { currencyFormat } from '../../utils/reusableFunctions';
 import Swal from 'sweetalert2';
 import './roomCard.scss';
 
@@ -14,7 +15,7 @@ const RoomCard = ({showMoreBtn=true, ...props}) => {
             <div className='card-col-1'>
                 <h2>{props.name}</h2>
                 
-                <p>${props.cost}</p>
+                <p>{currencyFormat(props.cost)}</p>
                     
             </div>
             <div className='card-col-2'>
--- caeq-web-portal/src/screens/Courses/Course.js
+++ caeq-web-portal/src/screens/Courses/Course.js
@@ -4,6 +4,7 @@ import { getCourse } from '../../client/Course/Course.GET';
 import { createInscription } from '../../client/Inscription/Inscription.POST';
 import { startPayment } from '../../client/Payment/Payment.POST'; // Importa la función para iniciar el proceso de pago
 import { FireError, FireSucess, FireLoading } from '../../utils/alertHandler';
+import { currencyFormat } from '../../utils/reusableFunctions';
 import BaseButton from '../../components/buttons/BaseButton';
 import ClassroomIcon from '../../components/icons/Classroom.png';
 import LocationIcon from '../../components/icons/Location.png';
@@ -83,7 +84,7 @@ const Course = (props) => {
             <div className='course-row'>
                 <h1>{data.courseName}</h1>
                 <h2 className='course-price'>
-                    {data.price ? `$${data.price}` : 'Gratuito'}
+                    {data.price ? `${currencyFormat(data.price)}` : 'Gratuito'}
                 </h2>
                 <RestrictByRole allowedRoles={['caeq']}>
                     <BaseButton
@@ -169,7 +170,7 @@ const Course = (props) => {
                                     <hr></hr>
                                     <h3>Costo del Curso</h3>
                                     <h2 className='course-price'>
-                                        {data.price ? `$${data.price}` : 'Gratuito'}
+                                        {data.price ? `${currencyFormat(data.price)}` : 'Gratuito'}
                                     </h2>
                                     <hr></hr>
                                     <FileInput

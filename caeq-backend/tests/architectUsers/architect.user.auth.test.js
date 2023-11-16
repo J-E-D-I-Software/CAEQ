0cb76f8f8b07d1c22f229777b0142be4e65f0a35
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-11-15 22:01:11 -0600

    Fixed Unit tests

--- caeq-backend/tests/architectUsers/architect.user.auth.test.js
+++ caeq-backend/tests/architectUsers/architect.user.auth.test.js
@@ -22,7 +22,7 @@ const testArchitectLogin = async () => {
 
     expect(resTest2.statusCode).toEqual(401);
     expect(resTest2.body.message).toEqual(
-        'Contraseña incorrecta. Intente de nuevo por favor. Si te registraste recientemente, por favor espere a que un administrador verifique  perfil.'
+        'Contraseña incorrecta. Intente de nuevo por favor. Si te registraste recientemente, por favor espere a que un administrador verifique su perfil.'
     );
 };
 
@@ -35,7 +35,7 @@ const testArchitectSignUp = async () => {
         .field('email', 'cesar@example.com')
         .field('password', password)
         .field('passwordConfirm', password)
-        .field('collegiateNumber', 45672)
+        .field('collegiateNumber', 90801)
         .field('memberType', 'Miembro de número')
         .field('classification', 'Docente')
         .field('DRONumber', 'DRO98765')
@@ -103,10 +103,9 @@ const testArchitectSignUp = async () => {
         .field('positionsInCouncil', 'Vocal')
         .field('capacitationHours', 90);
 
-    expect(resTest2.statusCode).toEqual(500);
+    expect(resTest2.statusCode).toEqual(200);
     expect(resTest2.body.message).toEqual(
-        'Una persona ya se ha inscrito en el portal con estos datos. ' +
-            'Crea una nueva cuenta o si crees que es un error contacta a gerencia.'
+        'Te has registrado con éxito, espera a que un administrador verifique que eres el arquitecto con el número de colegiado 45672 y te de acceso al portal.'
     );
 
     const resTest3 = await agent
@@ -195,37 +194,37 @@ const testArchitectSignUpNew = async () => {
 
 const testRegistrationCreation = async () => {
     const password = 'password789';
-    const resTest1 = await agent.post('/architectusers/auth/signup').send({
-        fullName: 'Pablo Jimenez',
-        email: 'pablito@example.com',
-        password: password,
-        passwordConfirm: password,
-        collegiateNumber: 45672,
-        memberType: 'Miembro de número',
-        classification: 'Docente',
-        DRONumber: 'DRO98765',
-        authorizationToShareInfo: true,
-        lifeInsurance: false,
-        lifeInsureID: '9937557b',
-        age: 40,
-        gender: 'Hombre',
-        cellphone: 5551112222,
-        homePhone: 5553334444,
-        officePhone: 5555556666,
-        emergencyContact: 'Ana García 5557778888',
-        mainProfessionalActivity: 'Ingeniero Civil',
-        dateOfAdmission: 2002,
-        dateOfBirth: new Date('1983-07-20'),
-        municipalityOfLabor: 'Querétaro',
-        linkCV: 'https://example.com/luisgarcia-cv',
-        university: 'Universidad Autónoma de Querétaro',
-        professionalLicense: 'P98765',
-        workAddress: '123 Avenida Principal, Querétaro',
-        homeAddress: '456 Calle Secundaria, Querétaro',
-        specialty: 'Corresponsable en seguridad estructural',
-        positionsInCouncil: 'Vocal',
-        capacitationHours: 90,
-    });
+    const resTest1 = await agent
+        .post('/architectusers/auth/signup')
+        .type('multipart/form-data')
+        .field('fullName', 'Pablo Jimenez')
+        .field('email', 'pablito@example.com')
+        .field('password', password)
+        .field('passwordConfirm', password)
+        .field('collegiateNumber', 45672)
+        .field('memberType', 'Miembro de número')
+        .field('classification', 'Docente')
+        .field('DRONumber', 'DRO98765')
+        .field('authorizationToShareInfo', true)
+        .field('lifeInsurance', false)
+        .field('lifeInsureID', '9937557b')
+        .field('age', 40)
+        .field('gender', 'Hombre')
+        .field('cellphone', 5551112222)
+        .field('homePhone', 5553334444)
+        .field('officePhone', 5555556666)
+        .field('emergencyContact', 'Ana García 5557778888')
+        .field('mainProfessionalActivity', 'Ingeniero Civil')
+        .field('dateOfAdmission', 2002)
+        .field('dateOfBirth', new Date('1983-07-20').toISOString())
+        .field('municipalityOfLabor', 'Querétaro')
+        .field('university', 'Universidad Autónoma de Querétaro')
+        .field('professionalLicense', 'P98765')
+        .field('workAddress', '123 Avenida Principal, Querétaro')
+        .field('homeAddress', '456 Calle Secundaria, Querétaro')
+        .field('specialty', 'Corresponsable en seguridad estructural')
+        .field('positionsInCouncil', 'Vocal')
+        .field('capacitationHours', 90);
 
     expect(resTest1.statusCode).toEqual(200);
     expect(resTest1.body).toBeTruthy();
--- caeq-backend/tests/architectUsers/architect.user.register.test.js
+++ caeq-backend/tests/architectUsers/architect.user.register.test.js
@@ -40,37 +40,38 @@ const testRejectNewUser = async () => {
 
 const testPendingProfileNoLogin = async () => {
     const password = 'password789';
-    const resTest1 = await agent.post('/architectusers/auth/signup').send({
-        fullName: 'Jorge Castro',
-        email: 'pablin@tec.mx',
-        password: password,
-        passwordConfirm: password,
-        collegiateNumber: 45672,
-        memberType: 'Miembro de número',
-        classification: 'Docente',
-        DRONumber: 'DRO98765',
-        authorizationToShareInfo: true,
-        lifeInsurance: false,
-        lifeInsureID: '9937557b',
-        age: 40,
-        gender: 'Hombre',
-        cellphone: 5551112222,
-        homePhone: 5553334444,
-        officePhone: 5555556666,
-        emergencyContact: 'Ana García 5557778888',
-        mainProfessionalActivity: 'Ingeniero Civil',
-        dateOfAdmission: 2002,
-        dateOfBirth: new Date('1983-07-20'),
-        municipalityOfLabor: 'Querétaro',
-        linkCV: 'https://example.com/luisgarcia-cv',
-        university: 'Universidad Autónoma de Querétaro',
-        professionalLicense: 'P98765',
-        workAddress: '123 Avenida Principal, Querétaro',
-        homeAddress: '456 Calle Secundaria, Querétaro',
-        specialty: 'Corresponsable en seguridad estructural',
-        positionsInCouncil: 'Vocal',
-        capacitationHours: 90,
-    });
+    const resTest1 = await agent
+        .post('/architectusers/auth/signup')
+        .type('multipart/form-data')
+        .field('fullName', 'Jorge Castro')
+        .field('email', 'pablin@tec.mx')
+        .field('password', password)
+        .field('passwordConfirm', password)
+        .field('collegiateNumber', 45672)
+        .field('memberType', 'Miembro de número')
+        .field('classification', 'Docente')
+        .field('DRONumber', 'DRO98765')
+        .field('authorizationToShareInfo', true)
+        .field('lifeInsurance', false)
+        .field('lifeInsureID', '9937557b')
+        .field('age', 40)
+        .field('gender', 'Hombre')
+        .field('cellphone', 5551112222)
+        .field('homePhone', 5553334444)
+        .field('officePhone', 5555556666)
+        .field('emergencyContact', 'Ana García 5557778888')
+        .field('mainProfessionalActivity', 'Ingeniero Civil')
+        .field('dateOfAdmission', 2002)
+        .field('dateOfBirth', new Date('1983-07-20').toISOString())
+        .field('municipalityOfLabor', 'Querétaro')
+        .field('linkCV', 'https://example.com/luisgarcia-cv')
+        .field('university', 'Universidad Autónoma de Querétaro')
+        .field('professionalLicense', 'P98765')
+        .field('workAddress', '123 Avenida Principal, Querétaro')
+        .field('homeAddress', '456 Calle Secundaria, Querétaro')
+        .field('specialty', 'Corresponsable en seguridad estructural')
+        .field('positionsInCouncil', 'Vocal')
+        .field('capacitationHours', 90);
 
     expect(resTest1.statusCode).toEqual(200);
     expect(resTest1.body).toBeTruthy();
@@ -86,43 +87,44 @@ const testPendingProfileNoLogin = async () => {
     expect(resLoginTest.statusCode).toEqual(401);
     expect(resLoginTest.body).toBeTruthy();
     expect(resLoginTest.body.message).toEqual(
-        'Email incorrecto. No hay un usuario registrado con este correo. Si te registraste recientemente, por favor espera a que un administrador verifique tu perfil.'
+        'Email incorrecto. No hay un usuario registrado con este correo. Si se registró recientemente, por favor espere a que un administrador verifique su perfil.'
     );
 };
 
 const testAcceptedProfileLogin = async () => {
     const password = 'password789';
-    const resTest1 = await agent.post('/architectusers/auth/signup').send({
-        fullName: 'Pablo Jimenez',
-        email: 'correoaceptado@example.com',
-        password: password,
-        passwordConfirm: password,
-        collegiateNumber: 45672,
-        memberType: 'Miembro de número',
-        classification: 'Docente',
-        DRONumber: 'DRO98765',
-        authorizationToShareInfo: true,
-        lifeInsurance: false,
-        lifeInsureID: '9937557b',
-        age: 40,
-        gender: 'Hombre',
-        cellphone: 5551112222,
-        homePhone: 5553334444,
-        officePhone: 5555556666,
-        emergencyContact: 'Ana García 5557778888',
-        mainProfessionalActivity: 'Ingeniero Civil',
-        dateOfAdmission: 2002,
-        dateOfBirth: new Date('1983-07-20'),
-        municipalityOfLabor: 'Querétaro',
-        linkCV: 'https://example.com/luisgarcia-cv',
-        university: 'Universidad Autónoma de Querétaro',
-        professionalLicense: 'P98765',
-        workAddress: '123 Avenida Principal, Querétaro',
-        homeAddress: '456 Calle Secundaria, Querétaro',
-        specialty: 'Corresponsable en seguridad estructural',
-        positionsInCouncil: 'Vocal',
-        capacitationHours: 90,
-    });
+    const resTest1 = await agent
+        .post('/architectusers/auth/signup')
+        .type('multipart/form-data')
+        .field('fullName', 'Pablo Jimenez')
+        .field('email', 'correoaceptado@example.com')
+        .field('password', password)
+        .field('passwordConfirm', password)
+        .field('collegiateNumber', 45672)
+        .field('memberType', 'Miembro de número')
+        .field('classification', 'Docente')
+        .field('DRONumber', 'DRO98765')
+        .field('authorizationToShareInfo', true)
+        .field('lifeInsurance', false)
+        .field('lifeInsureID', '9937557b')
+        .field('age', 40)
+        .field('gender', 'Hombre')
+        .field('cellphone', 5551112222)
+        .field('homePhone', 5553334444)
+        .field('officePhone', 5555556666)
+        .field('emergencyContact', 'Ana García 5557778888')
+        .field('mainProfessionalActivity', 'Ingeniero Civil')
+        .field('dateOfAdmission', 2002)
+        .field('dateOfBirth', new Date('1983-07-20').toISOString())
+        .field('municipalityOfLabor', 'Querétaro')
+        .field('linkCV', 'https://example.com/luisgarcia-cv')
+        .field('university', 'Universidad Autónoma de Querétaro')
+        .field('professionalLicense', 'P98765')
+        .field('workAddress', '123 Avenida Principal, Querétaro')
+        .field('homeAddress', '456 Calle Secundaria, Querétaro')
+        .field('specialty', 'Corresponsable en seguridad estructural')
+        .field('positionsInCouncil', 'Vocal')
+        .field('capacitationHours', 90);
 
     expect(resTest1.statusCode).toEqual(200);
     expect(resTest1.body).toBeTruthy();

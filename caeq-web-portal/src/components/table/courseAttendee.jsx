5e7a9c1661ce0860f43316085aae16148c0d4338
Sarah Navarro <sarah@MacBook-Air-de-Jorge-4.local>
2023-11-16 04:32:44 -0600

    fixed assistance Tables scss. Now only the table overflow and titles stay in place

--- caeq-web-portal/src/components/table/courseAttendee.jsx
+++ caeq-web-portal/src/components/table/courseAttendee.jsx
@@ -1,55 +1,91 @@
-import React, { useState } from 'react';
-import './InscriptionTable.scss';
-import BaseButton from '../buttons/BaseButton';
-import { Navigate } from 'react-router-dom';
-import AcceptIcon from '../icons/AcceptIcon.png';
-import RejectIcon from '../icons/RejectIcon.png';
+import React, { useState } from "react";
+// import './InscriptionTable.scss';
+import "./courseAttendee.scss";
+import BaseButton from "../buttons/BaseButton";
+import { Navigate } from "react-router-dom";
+import AcceptIcon from "../icons/AcceptIcon.png";
+import RejectIcon from "../icons/RejectIcon.png";
 
-const CourseAttendee = ({ data, userId }) => {
-    const attendedSessions = data.sort((a,b) => new Date(a.date) - new Date(b.date)).map((session) => {
-        return [new Date(session.date).toLocaleDateString(), session.attendees.includes(userId)]
-    }) 
-    console.log(attendedSessions)
-    const renderTableHeader = () => (
-        <tr>
-            {attendedSessions.map((sessionTouple) => (
-                <th key={sessionTouple[0]} className='sticky-column'>
-                    <div className='header-content'>
-                        <span className='header-text'>{sessionTouple[0]}</span>
-                    </div>
-                </th>
-            ))}
-        </tr>
-    );
+const CourseAttendee = ({ data, userId, course, hours }) => {
+  const attendedSessions = data
+    .sort((a, b) => new Date(a.date) - new Date(b.date))
+    .map((session) => {
+      return [
+        new Date(session.date).toLocaleDateString(),
+        session.attendees.includes(userId),
+      ];
+    });
+  const space =
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0" +
+    "\u00A0";
+  const totalSessions = attendedSessions.length;
+  const attendedCount = attendedSessions.filter(
+    ([_, attended]) => attended
+  ).length;
 
-    const renderTableBody = () => (
-        <tbody>
-            <tr key={userId}>
-                {attendedSessions.map((sessionTouple) => (
-                    <td key={sessionTouple[0]}>
-                        {sessionTouple[1] ? <img src={AcceptIcon} /> : <img src={RejectIcon} />}
-                    </td>
-                ))}
-            </tr>
-        </tbody>
-    );
+  const renderTableHeader = () => (
+    <tr>
+      {attendedSessions.map((sessionTouple) => (
+        <th key={sessionTouple[0]} className="sticky-column">
+          <div className="header-content">
+            <span className="header-text">{sessionTouple[0]}</span>
+          </div>
+        </th>
+      ))}
+    </tr>
+  );
 
-    return (
-        <div className='inscription-table'>
-            <div className='inscription-row'>
-                <h1>Asistencias</h1>
-            </div>
-            <div className='tabla-container'>
-            <div className='table-wrapper'>
-                <table className='tabla'>
-                    <thead>{renderTableHeader()}</thead>
-                    {renderTableBody()}
-                </table>
-            </div>
-        </div>
-        </div>
-        
-    );
+  const renderTableBody = () => (
+    <tbody>
+      <tr key={userId}>
+        {attendedSessions.map((sessionTouple) => (
+          <td key={sessionTouple[0]}>
+            {sessionTouple[1] ? (
+              <img src={AcceptIcon} />
+            ) : (
+              <img src={RejectIcon} />
+            )}
+          </td>
+        ))}
+      </tr>
+    </tbody>
+  );
+
+  return (
+    <div className="tabla-container">
+      <h1>Nombre del curso: {course}</h1>
+
+      <div className="table-wrapper">
+        <table className="tabla">
+          <thead>{renderTableHeader()}</thead>
+          {renderTableBody()}
+        </table>
+      </div>
+
+      <h2>
+        Horas totales acreditadas: {hours} {space} Mis asistencias a este curso:{" "}
+        {attendedCount} de {totalSessions}
+      </h2>
+    </div>
+  );
 };
 
 export default CourseAttendee;
